689505432d5c394b058873312a5f68830109f571
ontheroad1992 <ontheroad1992@qq.com>
2022-06-28 00:32:39 +0800

    rust=>feat: hello_cargo

---
+++ rust/hello_cargo/src/main.rs
@@ -0,0 +1,9 @@
+// cargo new my-project 创建项目名字
+// cargo build 打包项目
+//      被打包存在于 ./target/debug/ 下面
+// cargo run 一步构建并运行项目
+// cargo check 在不生成二进制文件的情况下构建项目来检查错误
+
+fn main() {
+    println!("Hello, world!");
+}
