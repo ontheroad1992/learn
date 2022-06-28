cad8e116e439517657c992cd063a2ac51b81d2b5
v_pengphe <v_pengphe@tencent.com>
2022-06-28 19:45:50 +0800

    rust=>variables: 基本的数据类型了解

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -1,11 +1,13 @@
+use std::io;
+
 fn main() {
     // mut let 的可变性
     // let mut x = 5;
-    // p&rintln!("the value of x is:{}", x);
+    // p&&&rintln!("the value of x is:{}", x);
     // x = 6;
     // println!("the value of x is:{}", x)
 
-    // let 的作用域
+    // let 的作&用域
     let x = 5;
 
     let x = x + 1;
@@ -23,4 +25,66 @@ fn main() {
 
     // let mut spaces = "   ";
     // spaces = spaces.le(); // error 前后的类型不同
+
+    let guess: u32 = "42".parse().expect("Not a number");
+    println!("guess is: {}", guess);
+
+    let sum = 5 + 10;
+    println!("sum: {}", sum);
+
+    let difference: f32 = 90.2 - 2.1;
+    println!("difference: {}", difference);
+    // let result: f32 = difference;
+    // println!("difference: {}", result);
+
+    let product = 4 * 30;
+    println!("product: {}", product);
+
+    let quotient: f32 = 56.7 / 10.0;
+    let floored = 2 / 3;
+    println!("quotient: {}", quotient);
+    println!("floored: {}", floored);
+
+    // 取余
+    let remainder = 43 % 5;
+    println!("remainder: {}", remainder);
+
+    {
+        // 元组类型
+        let tup: (i32, f64, u8) = (500, 9.2, 8);
+        println!("tup: {}", tup.0);
+
+        let (x, y, z) = tup;
+        println!("tup: {} {} {}", x, y, z)
+    }
+
+    {
+        // 数组
+        let a: [i32; 5] = [1, 2, 3, 4, 5];
+        println!("arr: {}", a[1])
+    }
+
+    {
+        // 无效的数组元素访问
+        let a = [1, 2, 3, 4, 5];
+        println!("Please enter an array index.");
+
+        let mut index = String::new();
+
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to read line");
+
+        let index: usize = index
+            .trim()
+            .parse()
+            .expect("index entered was not a number");
+
+        let element = a[index];
+
+        println!(
+            "The value of the element at index {} is: {}",
+            index, element
+        )
+    }
 }
