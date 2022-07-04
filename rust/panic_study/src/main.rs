a1d9cc7e6dd48b6cc659819996f9cf8ab112697e
v_pengphe <v_pengphe@tencent.com>
2022-07-04 20:01:20 +0800

    rust=>panic: Result 以及 ？运算的用法

--- rust/panic_study/src/main.rs
+++ rust/panic_study/src/main.rs
@@ -1,7 +1,17 @@
-use std::{fs::File, io::ErrorKind};
+use std::{
+    fs::{self, File},
+    io::{self, ErrorKind, Read},
+};
 
 fn main() {
-    let f = File::open("hello.txt").unwrap();
+    // let f = File::open("hello.txt").unwrap();
+
+    let f2 = read_username_from_file("hello.txt");
+
+    println!("f2: {:?}", f2);
+
+    // let f3: &str = f;
+    // println!("line: {:?}", lat_char_of_first_line(f2))
 }
 
 fn open_file_or_create() {
@@ -32,4 +42,45 @@ fn other_fn() {
             panic!("problem opening the file: {:?}", error)
         }
     });
+}
+
+fn read_username_from_file(path: &str) -> Result<String, io::Error> {
+    // let mut f = File::open(path);
+
+    // let mut f = match f {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e),
+    // };
+
+    // let mut s = String::new();
+
+    // match f.read_to_string(&mut s) {
+    //     Ok(_) => Ok(s),
+    //     Err(e) => Err(e),
+    // }
+
+    // 变行1
+    // let mut f = File::open(path)?;
+
+    // let mut s = String::new();
+
+    // f.read_to_string(&mut s)?;
+
+    // Ok(s)
+
+    // 变形2
+    // let mut s = String::new();
+
+    // File::open(path)?.read_to_string(&mut s)?;
+
+    // Ok(s)
+
+    // ? 运算符号只能应用于 Result 的返回 或者 Option
+
+    // 最直接的方式
+    fs::read_to_string(path)
+}
+
+fn lat_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
