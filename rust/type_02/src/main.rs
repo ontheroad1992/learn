31302df0a6602f951858503d94ba443f69a0eff6
v_pengphe <v_pengphe@tencent.com>
2022-10-19 11:43:42 +0800

    refactor: 所有权练习

--- rust/type_02/src/main.rs
+++ rust/type_02/src/main.rs
@@ -8,7 +8,11 @@ fn main() {
     test4();
     test5();
     // 函数部分
-    println!("{}", plus_five(1))
+    println!("{}", plus_five(1));
+    str_test();
+    println!("借用与引用");
+    ownership();
+    ownership2();
 }
 
 // fn test1() {
@@ -61,3 +65,57 @@ fn plus_five(x: i32) -> i32 {
 // fn report<T: std::fmt::Debug>(item: T) {
 //     println!("{:?}", item);
 // }
+
+fn str_test() {
+    let mut s = String::from("hello world");
+    s.push_str("string");
+    println!("{}", s);
+
+    let mut s2 = s.clone(); // 深拷贝
+    s2.push_str(" test");
+    println!("s: {}, s2: {}", s, s2);
+
+    let x = 5;
+    makes_copy(x);
+    println!("x: {}", x)
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("some_integer: {}", some_integer)
+}
+
+fn ownership() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let s1 = String::from("hello world");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}", s1, len);
+}
+
+// 借用只有使用权，没有修改的权利
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn ownership2() {
+    let mut s = String::from("hello");
+    change(&mut s);
+
+    println!("ownership2: {}", s);
+    {
+        let r1 = &mut s;
+        r1.push_str(" string");
+
+        println!("r1: {}", r1);
+    }
+
+    println!("sdasdasdas: {}", s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(" world")
+}
